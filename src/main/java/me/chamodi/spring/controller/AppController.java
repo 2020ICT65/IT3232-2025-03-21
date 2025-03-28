580a1aa529f84ab939ed88c8947dcdd3d6d81edd
chamoindrejith <104416340+chamoindrejith@users.noreply.github.com>
2025-03-28 08:54:48 +0530

    Access data through path variables

--- src/main/java/me/chamodi/spring/controller/AppController.java
+++ src/main/java/me/chamodi/spring/controller/AppController.java
@@ -1,6 +1,7 @@
 package me.chamodi.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,9 @@ public class AppController {
 	public String myName() {
 		return "Hello I'm Chamodi";
 	}
+	@GetMapping("/age/{ag}")
+	public String myAge(@PathVariable("ag") int age) {
+		return "My age is "+age;
+	}
+	
 }
